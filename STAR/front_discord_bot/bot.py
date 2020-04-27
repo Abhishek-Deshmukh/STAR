1026c2ee4bf4d6f3e6b620d5cc2e42e605c48b56
Abhishek-Deshmukh <deshmukhabhishek369@gmail.com>
2020-04-28 04:30:05 +0530

    the bot can run pre-decided command by orders from owner and give back the id

--- STAR/front_discord_bot/bot.py
+++ STAR/front_discord_bot/bot.py
@@ -3,6 +3,7 @@ Abhishek Anil Deshmukh <deshmukhabhishek369@gmail.com>
 the bot for sending and accepting message to and from discord
 and communicate with the api
 """
+import requests
 from discord.ext import commands
 
 # setting up the bot
@@ -15,17 +16,36 @@ async def on_ready():
     """
     print("BOT trainer is ready and in working conditon")
 
+
 @BOT.command()
 async def ping(ctx):
     """To if working without messing it up
     """
     await ctx.send("pong")
 
+
 @BOT.command()
 async def fuck(ctx):
     """To if working without messing it up(vulgar version)
     """
     await ctx.send("you")
+
+
+@BOT.command()
+async def run(ctx):
+    """TO run the command from discord via the bot
+    """
+    await ctx.send("starting... 🤔")
+    params = {
+        "f": "sqrt(4 - xs**2)",
+        "a": "0",
+        "b": "2",
+        "c": "0",
+        "d": "2",
+        "size": "1000",
+    }
+    req = requests.put("http://localhost:5000", json=params)
+    await ctx.send(f"process has started:\n{req.text}")
 
 
 # starting the trainer
